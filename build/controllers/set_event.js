47fc0bccc7309faa047746456145ae91126f2f5a
Omkumar <74484315+Om25091210@users.noreply.github.com>
2023-12-23 13:42:33 +0530

    Updated Google Cred

--- build/controllers/set_event.js
+++ build/controllers/set_event.js
@@ -20,10 +20,10 @@ require("dotenv").config();
 // Your credentials from Google Cloud Console
 const credentials = {
     // Your client ID and client secret
-    client_id: "587504325771-2um6j3u4fieao3bpso064gj32po77or1.apps.googleusercontent.com",
-    client_secret: "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",
+    client_id: "587504325771-5ci8erb2ihfkq37fjd23qidsfl57mnjg.apps.googleusercontent.com",
+    client_secret: "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",
     // Redirect URIs should match the one you set in the Google Cloud Console
-    redirect_uris: ['https://3b20-2409-4081-ab99-e449-3c31-1398-c251-393e.ngrok-free.app/auth-callback'],
+    redirect_uris: ['https://productive-gpt.vercel.app/auth-callback'],
 };
 // Now you can use this redirectUri in your OAuth URL
 const oAuth2Client = new googleapis_1.google.auth.OAuth2(credentials.client_id, credentials.client_secret, credentials.redirect_uris[0]);
